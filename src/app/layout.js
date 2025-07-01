e3709ac868a4ed6ec29f2454ef62be6765ba7f01
Pasinduroshan <roshanpasindu@gmail.com>
2025-07-01 12:39:22 +0530

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import TransitionProvider from "@/components/transitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
